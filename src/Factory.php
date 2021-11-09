5135f4969d8ad49b52602b1a7de4d0f8abb16311
zhanruijia <227040015@qq.com>
2021-11-09 17:50:31 +0800

    v1

---
+++ src/Factory.php
@@ -0,0 +1,28 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |      Site: www.hdcms.com
+ * |      Date: 2018/6/25 下午3:13
+ * |    Author: 向军大叔 <2300071698@qq.com>
+ * '-------------------------------------------------------------------*/
+
+namespace Georgie\AutoCreate;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class Facade
+ *
+ * @package Georgie\Module
+ */
+class Factory extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'HDAutoCreate';
+    }
+}
---
+++ src/Provider.php
@@ -0,0 +1,18 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |      Site: www.hdcms.com
+ * |      Date: 2018/6/25 下午3:13
+ * |    Author: 向军大叔 <2300071698@qq.com>
+ * '-------------------------------------------------------------------*/
+
+namespace Georgie\AutoCreate;
+
+/**
+ * Class Facade
+ *
+ * @package Georgie\Module
+ */
+class Provider
+{
+
+}
