4c307f02283ad96002916d4b91096f085eab58b4
sihui <40323239+georgie233@users.noreply.github.com>
2021-11-13 14:12:55 +0800

    Update image.blade.php

--- src/Build/forms/image.blade.php
+++ src/Build/forms/image.blade.php
@@ -1,10 +1,10 @@
 <div class="form-group row">
     <label for="{column['name']}" class="col-12 col-sm-3 col-form-label text-md-right">{column['title']}</label>
     <div class="col-12 col-lg-9 img-up">
-        <div id="{column['name']}-btn" class="btn btn-success">
-            上传图片
-        </div>
-        <input id="{column['name']}-input" name="{column['name']}" accept=".jpg,.png,.bmp,.jpeg" type="file" hidden />
+{{--                        <div id="thumb-btn" class="btn btn-success">--}}
+{{--                            上传图片--}}
+{{--                        </div>--}}
+        <input id="{column['name']}-input" name="{column['name']}" accept=".jpg,.png,.bmp,.jpeg" type="file" />
         <img style="max-width: 120px;" id="{column['name']}-img" src="" alt="">
         <div class="{{ $errors->has('{column['name']}') ? ' is-invalid' : '' }}"></div>
         @if ($errors->has('{column['name']}'))
