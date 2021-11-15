0006f9e12165efba0b102d4e4d80ff3f196841df
sihui <40323239+georgie233@users.noreply.github.com>
2021-11-15 13:59:21 +0800

    Update image.blade.php

--- src/Build/forms/image.blade.php
+++ src/Build/forms/image.blade.php
@@ -1,11 +1,9 @@
 <div class="form-group row">
     <label for="{column['name']}" class="col-12 col-sm-3 col-form-label text-md-right">{column['title']}</label>
     <div class="col-12 col-lg-9 img-up">
-{{--                        <div id="thumb-btn" class="btn btn-success">--}}
-{{--                            上传图片--}}
-{{--                        </div>--}}
-        <input id="{column['name']}-input" name="{column['name']}" accept=".jpg,.png,.bmp,.jpeg" type="file" />
-        <img style="max-width: 120px;" id="{column['name']}-img" src="" alt="">
+{{--        <label><span class="btn-big btn-success">重新上传</span><input id="{column['name']}-input" name="{column['name']}" accept=".jpg,.png,.jpeg" type="file" hidden/></label>--}}
+        <input id="{column['name']}-input" name="{column['name']}" accept=".jpg,.png,.jpeg" type="file" />
+        <img style="max-width: 120px;" id="{column['name']}-img" src="{{ ${SMODEL}['{column['name']}'] }}" alt="">
         <div class="{{ $errors->has('{column['name']}') ? ' is-invalid' : '' }}"></div>
         @if ($errors->has('{column['name']}'))
             <span class="text-danger">
