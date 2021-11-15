0bbc3d65bcf6409f07ba9072becfeea01e4a7d64
sihui <40323239+georgie233@users.noreply.github.com>
2021-11-15 14:43:32 +0800

    Update index.blade.php

--- src/Build/views/index.blade.php
+++ src/Build/views/index.blade.php
@@ -1,11 +1,5 @@
 @extends('admin::layouts.master')
 @section('content')
-    <script type="text/javascript">if (window.innerWidth < 600) {
-            window.location.href = window.location.href;
-            let dom = document.getElementsByClassName('alert-msg');
-            if (dom)localStorage.setItem('dom',dom[0].innerHTML);
-        }
-    </script>
     <div class="card" id="app">
         <div class="card-header">{MODEL_TITLE}管理</div>
         <div class="tab-container">
@@ -60,3 +54,11 @@
         }
     </script>
 @endsection
+@section('content_script')
+    <script type="text/javascript">if (window.innerWidth < 600) {
+            window.location.href = window.location.href;
+            let dom = document.getElementsByClassName('alert-msg');
+            if (dom) localStorage.setItem('dom', dom[0].innerHTML);
+        }
+    </script>
+@stop
